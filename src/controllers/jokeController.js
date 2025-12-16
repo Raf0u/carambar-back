9be287478f7841723e78e2c42e342cff48670285
Nicolas Hubert <nicolas.hubert@zalando.de>
2025-12-16 17:49:21 +0100

    add function for fetching all jokes

--- src/controllers/jokeController.js
+++ src/controllers/jokeController.js
@@ -5,10 +5,14 @@ const sequelize = require('../db/sequelize'); // pour la fonction random()
 // Récupérer toutes les blagues
 exports.findAllJokes = (req, res) => {
     Joke.findAll()
+        // .then(...) pour gérer le résultat si l'opération a réussi
         .then(jokes => {
+            // convertit le tableau d'objets jokes en une chaîne JSON et l'envoie au client
             res.json(jokes);
         })
+        // .catch(...) pour gérer l'échec de la Promise
         .catch(error => {
+            // envoie une réponse JSON au client, incluant l'objet error lui-même (pour faciliter le débogage)
             res.status(500).json({ message: "Erreur lors de la récupération des blagues.", error });
         });
 };
